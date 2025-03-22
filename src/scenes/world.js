e6c7856a54012721d1bcb6e5e26f214bf827a82d
Olha Shtyfliuk <149540630+fwrwxx@users.noreply.github.com>
2025-03-22 12:46:11 +0200

    made the camera follow the player

--- src/scenes/world.js
+++ src/scenes/world.js
@@ -1,10 +1,18 @@
-import { generatePlayerComponents } from "../entities/player.js";
-import { generateSlimeComponents } from "../entities/slime.js";
-import { colorizeBackground, drawBoundaries, drawTiles, fetchMapData } from "../utils.js";
+import {
+  generatePlayerComponents,
+  setPlayerMovement,
+} from '../entities/player.js';
+import { generateSlimeComponents } from '../entities/slime.js';
+import {
+  colorizeBackground,
+  drawBoundaries,
+  drawTiles,
+  fetchMapData,
+} from '../utils.js';
 
 export default async function world(k) {
   colorizeBackground(k, 76, 170, 255);
-  const mapData = await fetchMapData("./assets/maps/world.json");
+  const mapData = await fetchMapData('./assets/maps/world.json');
   console.log(mapData);
 
   const map = k.add([k.pos(0, 0)]);
@@ -16,21 +24,24 @@ export default async function world(k) {
 
   const layers = mapData.layers;
   for (const layer of layers) {
-    if (layer.name === "boundaries") {
+    if (layer.name === 'boundaries') {
       drawBoundaries(k, map, layer);
       continue;
     }
 
-    if (layer.name === "spawn-points") {
+    if (layer.name === 'spawn-points') {
       for (const object of layer.objects) {
-        if (object.name === "player") {
-          entities.player = map.add(generatePlayerComponents(k, k.vec2(object.x, object.y)));
+        if (object.name === 'player') {
+          entities.player = map.add(
+            generatePlayerComponents(k, k.vec2(object.x, object.y)),
+          );
           continue;
         }
 
-        if (object.name === "slime") {
-          entities.slimes.push(map.add(generateSlimeComponents(k, k.vec2(object.x, object.y))));
-
+        if (object.name === 'slime') {
+          entities.slimes.push(
+            map.add(generateSlimeComponents(k, k.vec2(object.x, object.y))),
+          );
         }
       }
       continue;
@@ -40,5 +51,20 @@ export default async function world(k) {
   }
 
   k.camScale(4);
-  k.camPos(entities.player.worldPos())
+  k.camPos(entities.player.worldPos());
+  k.onUpdate(() => {
+    if (entities.player.pos.dist(k.camPos())) {
+      k.tween(
+        k.camPos(),
+        entities.player.worldPos(),
+        0.15,
+        (newPos) => {
+          k.camPos(newPos);
+        },
+        k.easings.linear,
+      );
+    }
+  });
+
+  setPlayerMovement(k, entities.player);
 }
