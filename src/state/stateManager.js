8fc94978041c086a254e17b6472080519bd3198b
Olha Shtyfliuk <149540630+fwrwxx@users.noreply.github.com>
2025-03-27 15:44:18 +0200

    update

--- src/state/stateManager.js
+++
@@ -1,7 +0,0 @@
-import globalStatemanager from './globalState.js';
-import oldManGlobalStateManager from './oldmanGlobalState.js';
-import playerGlobalStateManager from "./playerGlobalState.js";
-
-export const gameState = globalStatemanager().getInstance();
-export const oldmanState = oldManGlobalStateManager().getInstance();
-export const playerState = playerGlobalStateManager().getInstance();
