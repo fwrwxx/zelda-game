abdd9559cf0f8e2ae4350e07ce5bd03d48742a46
Olha Shtyfliuk <149540630+fwrwxx@users.noreply.github.com>
2025-05-05 15:04:03 +0300

    final check

---
+++ src/content/oldmanDialogue.js
@@ -0,0 +1,38 @@
+const english = [
+  [
+    "Please, rescue my son! He's locked away in a dungeon to the west.",
+    'It’s too risky to go by yourself!',
+    'Here, take this sword. Press space to swing it.',
+    "I'll reward you generously for your bravery!",
+  ],
+  [
+    'Need a reminder on how to use your sword?',
+    'Press the spacebar to strike.',
+    "Now, hurry and rescue my son before it's too late!",
+  ],
+  ['Please, save my son!'],
+  ['As a token of my gratitude, you may keep the sword I gave you.'],
+];
+
+const ukrainian = [
+  [
+    'Будь ласка, врятуй мого сина! Він замкнений у підземеллі на заході.',
+    'Занадто небезпечно йти туди одному!',
+    'Ось, візьми цей меч. Натисни пробіл, щоб атакувати',
+    'Я щедро винагороджу тебе за твою сміливість!',
+  ],
+  [
+    'Забув, як користуватися мечем?',
+    'Натисни пробіл для атаки.',
+    'Тепер поспіши і врятуй мого сина, поки ще не запізно!',
+  ],
+  ['Будь ласка, врятуй мого хлопчика!'],
+  ['У знак своєї безмежної подяки дарую тобі цей меч.'],
+];
+
+const oldmanLines = {
+  english,
+  ukrainian,
+};
+
+export default oldmanLines;
