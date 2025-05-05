fe3c9fbadc3aa375cbbd43b21b4546737aa088a4
Olha Shtyfliuk <149540630+fwrwxx@users.noreply.github.com>
2025-05-05 15:09:47 +0300

    Delete src/state/state-manager.js

--- src/state/state-manager.js
+++
@@ -1,7 +0,0 @@
-import globalStatemanager from './global-state.js';
-import oldManGlobalStateManager from './oldman-global-state.js';
-import playerGlobalStateManager from "./player-global-state.js";
-
-export const gameState = globalStatemanager().getInstance();
-export const oldmanState = oldManGlobalStateManager().getInstance();
-export const playerState = playerGlobalStateManager().getInstance();
